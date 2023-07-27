0280390aa91eafdfea1ad95cae711db9dccc9f0b
bvannier2021 <alucard84000@gmail.com>
2023-07-27 13:59:59 +0200

    Fin du tp1

---
+++ src/app/components/generations/generations.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generations',
+  templateUrl: './generations.component.html',
+  styleUrls: ['./generations.component.css']
+})
+export class GenerationsComponent {
+
+}
